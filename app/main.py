9c65c2b7e750e725038b3b4fca6800d7efeb762f
Rafal Mlynarski <rafal.mlynarski.82@gmail>
2026-03-01 19:10:24 +0100

    Fix Herbivores and carnivores

--- app/main.py
+++ app/main.py
@@ -1,28 +1,50 @@
-class Animal:
-    alive = []
+from __future__ import annotations
 
-    def __init__(self, name: str, health: int = 100, hidden: bool = False) -> None:
-        self.name = name
-        self.health = health
-        self.hidden = hidden
+
+class Animal:
+    alive: list["Animal"] = []
+
+    def __init__(
+        self,
+        name: str,
+        health: int = 100,
+        hidden: bool = False,
+    ) -> None:
+        self.name: str = name
+        self.health: int = health
+        self.hidden: bool = hidden
         Animal.alive.append(self)
 
-    def take_damage(self, amount: int) -> None:
-        self.health = self.health - amount
+    def take_damage(
+        self,
+        amount: int,
+    ) -> None:
+        self.health -= amount
         if self.health <= 0:
             self.health = 0
             Animal.alive.remove(self)
 
-    def __repr__(self) -> str:
-        return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
+    def __repr__(
+        self,
+    ) -> str:
+        return (
+            f"{{Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}"
+        )
 
 
 class Herbivore(Animal):
-    def hide(self) -> None:
+    def hide(
+        self,
+    ) -> None:
         self.hidden = not self.hidden
 
 
 class Carnivore(Animal):
-    def bite(self, other: Animal) -> None:
+    def bite(
+        self,
+        other: Animal,
+    ) -> None:
         if isinstance(other, Herbivore) and not other.hidden:
             other.take_damage(50)
